8aa8c0aa97e0fac5374fe579ff538cb423522322
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-09-17 19:42:20 +0800

    add owner routes to the application

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
-
+const ownerRoutes = require('./routes/owner.routes');
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -11,6 +11,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
+app.use('/api/owners', ownerRoutes);
 app.get('/', (req, res) => res.json({ message: 'VetCare API running' }));
 
 module.exports = app;
