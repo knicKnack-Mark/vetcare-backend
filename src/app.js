775793fd5d66ffc3022c407c39f02be4358c8e18
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-09-18 22:04:45 +0800

    feat: reorganize route imports and middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const ownerRoutes = require('./routes/owner.routes');
+const petRoutes = require('./routes/pet.routes');
+const appointmentRoutes = require('./routes/appointment.routes');
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use('/api/auth', authRoutes);
-
+app.use('/api/appointments', appointmentRoutes);
 app.use('/api/owners', ownerRoutes);
+app.use('/api/pets', petRoutes);
 app.get('/', (req, res) => res.json({ message: 'VetCare API running' }));
 
 module.exports = app;
