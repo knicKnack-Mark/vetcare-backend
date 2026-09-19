cd367142a2b2fc133c46d31631eb8ded02634a18
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-09-19 10:30:23 +0800

    feat: add vaccination routes to app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const authRoutes = require('./routes/auth.routes');
 const ownerRoutes = require('./routes/owner.routes');
 const petRoutes = require('./routes/pet.routes');
 const appointmentRoutes = require('./routes/appointment.routes');
+const vaccinationRoutes = require('./routes/vaccination.routes');
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -14,6 +15,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/owners', ownerRoutes);
 app.use('/api/pets', petRoutes);
+app.use('/api/vaccinations', vaccinationRoutes);
+
+
 app.get('/', (req, res) => res.json({ message: 'VetCare API running' }));
 
 module.exports = app;
