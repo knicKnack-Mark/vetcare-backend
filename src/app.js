badd849cca37b68730ddcf58611a3a74298bdc92
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-09-19 11:52:21 +0800

    feat: add user and deworming routes to the API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const ownerRoutes = require('./routes/owner.routes');
 const petRoutes = require('./routes/pet.routes');
 const appointmentRoutes = require('./routes/appointment.routes');
 const vaccinationRoutes = require('./routes/vaccination.routes');
+const userRoutes = require('./routes/user.routes');
+const dewormingRoutes = require('./routes/deworming.routes');
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -16,8 +18,8 @@ app.use('/api/appointments', appointmentRoutes);
 app.use('/api/owners', ownerRoutes);
 app.use('/api/pets', petRoutes);
 app.use('/api/vaccinations', vaccinationRoutes);
-
-
+app.use('/api/users', userRoutes);
+app.use('/api/dewormings', dewormingRoutes);
 app.get('/', (req, res) => res.json({ message: 'VetCare API running' }));
 
 module.exports = app;
